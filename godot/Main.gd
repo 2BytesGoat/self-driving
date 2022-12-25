6a7f73cdf22a82e24a218893150a19f84681f288
GianiStatie <gianistatie@gmail.com>
2022-12-25 21:23:07 +0200

    Added calculation for laps

---
+++ godot/Main.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var curve = get_node("Path2D").get_curve()
+onready var agent = get_node("Agent")
+
+var prev_perc
+
+func _ready():
+	prev_perc = get_completion_perc()
+
+func _input(event):
+	if event is InputEventKey:
+		if Input.is_action_just_pressed("ui_accept"):
+			var perc = get_completion_perc()
+			if prev_perc >= 0.95 and perc <= 0.1:
+				print('score')
+			if prev_perc <= 0.1 and perc >= 0.95:
+				print('undo point')
+			prev_perc = perc
+
+func get_completion_perc():
+	var agent_position = agent.global_position
+	var offset = curve.get_closest_offset(agent_position)
+	return offset/curve.get_baked_length()
