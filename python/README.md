1ca8bf8b020f257891e09c849de219f3deca509a
GianiStatie <gianistatie@gmail.com>
2022-12-31 17:46:46 +0200

    Added docs for Python

---
+++ python/README.md
@@ -0,0 +1,28 @@
+# How to use
+
+## 1. Open Godot project and start main scene
+![gif](docs/Animation.gif)
+
+## 2. Install requirements
+1.1. Create python environment
+```
+python -m venv .env
+```
+1.2. Install dependencies
+```
+# Ubuntu
+source .env/bin/activate
+pip install -r requirements.txt
+
+# Windows
+.env/Scripts/activate
+pip install -r requirements.txt
+```
+1.3. (Optional) Install Graphviz \
+See link: https://graphviz.org/download/
+
+## 3. Start Python script 
+Run in the same python environment as above
+```
+python main.py
+```
--- python/main.py
+++ python/main.py
@@ -1,27 +1,32 @@
 import os
+import typer
 
 from godot_environment import GodotEnv
 from neat_trainer import NeatTrainer
  
-EPISODE_LEN = 1000
-GENERATIONS = 300
-CONFIG_FILE = "configs/neat.cfg"
-CHECKPOINT_FILE = None
+def main(
+    episode_len:int = 1000, 
+    generations:int = 300, 
+    config_file:str = "configs/neat.cfg", 
+    checkpoint_file:str = None
+    ):
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, config_file)
+    checkpoint_path = None if not checkpoint_file else os.path.join(local_dir, checkpoint_file)
 
-local_dir = os.path.dirname(__file__)
-config_path = os.path.join(local_dir, CONFIG_FILE)
-checkpoint_path = None if not CHECKPOINT_FILE else os.path.join(local_dir, CHECKPOINT_FILE)
+    env = GodotEnv()
+    neat = NeatTrainer(env, config_path, checkpoint_path, episode_len)
+    winner = neat.find_winner(generations)
 
-env = GodotEnv()
-neat = NeatTrainer(env, config_path, checkpoint_path, EPISODE_LEN)
-winner = neat.find_winner(GENERATIONS)
+    # TODO: add call to get node names from Godot
+    node_names = {
+        -1: 'Agent Rotation', 
+        -2: 'Trajectory Rad',
+        -3: 'Distance', 
+        0: 'Direction X',
+        1: 'Direction Y'
+    }
+    neat.visualize_winner(winner, node_names)
 
-# TODO: add call to get node names from Godot
-node_names = {
-    -1: 'Agent Rotation', 
-    -2: 'Trajectory Rad',
-    -3: 'Distance', 
-    0: 'Direction X',
-    1: 'Direction Y'
-}
-neat.visualize_winner(winner, node_names)
+if __name__ == "__main__":
+    typer.run(main)
